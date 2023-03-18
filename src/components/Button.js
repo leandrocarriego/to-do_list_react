9feacdf2ed7c99942a93ad49e4a0ec4ab718d14a
Leandro Carriego <leandrocarriego@hotmail.com>
2023-03-17 22:11:26 -0300

    first commit

---
+++ src/components/Button.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const Button = () => {
+  return (
+    <div>
+      boton
+    </div>
+  )
+}
+
+export default Button
---
+++ src/components/List.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const List = () => {
+  return (
+    <div>
+      Lista
+    </div>
+  )
+}
+
+export default List
